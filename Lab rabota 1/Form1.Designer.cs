0d59b3a7c9bf02247dd492a3217005c9bd0c1602
konnovegor <67024710+konnovegor@users.noreply.github.com>
2020-06-17 01:52:40 +0500

    Add files via upload

---
+++ Lab rabota 1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Lab_rabota_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbx = new System.Windows.Forms.TextBox();
+            this.tby = new System.Windows.Forms.TextBox();
+            this.tbz = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tba = new System.Windows.Forms.TextBox();
+            this.tbb = new System.Windows.Forms.TextBox();
+            this.tbg = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tbxy = new System.Windows.Forms.TextBox();
+            this.tbzx = new System.Windows.Forms.TextBox();
+            this.tbxz = new System.Windows.Forms.TextBox();
+            this.tbyx = new System.Windows.Forms.TextBox();
+            this.tbzy = new System.Windows.Forms.TextBox();
+            this.tbyz = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textbxx = new System.Windows.Forms.TextBox();
+            this.textbxz = new System.Windows.Forms.TextBox();
+            this.textbxy = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(430, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Масштаб";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Масштаб_Click);
+            // 
+            // tbx
+            // 
+            this.tbx.Location = new System.Drawing.Point(430, 46);
+            this.tbx.Name = "tbx";
+            this.tbx.Size = new System.Drawing.Size(100, 20);
+            this.tbx.TabIndex = 2;
+            // 
+            // tby
+            // 
+            this.tby.Location = new System.Drawing.Point(536, 46);
+            this.tby.Name = "tby";
+            this.tby.Size = new System.Drawing.Size(100, 20);
+            this.tby.TabIndex = 3;
+            // 
+            // tbz
+            // 
+            this.tbz.Location = new System.Drawing.Point(642, 46);
+            this.tbz.Name = "tbz";
+            this.tbz.Size = new System.Drawing.Size(100, 20);
+            this.tbz.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(430, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Поворот";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Поворот_Click);
+            // 
+            // tba
+            // 
+            this.tba.Location = new System.Drawing.Point(430, 106);
+            this.tba.Name = "tba";
+            this.tba.Size = new System.Drawing.Size(100, 20);
+            this.tba.TabIndex = 6;
+            // 
+            // tbb
+            // 
+            this.tbb.Location = new System.Drawing.Point(536, 106);
+            this.tbb.Name = "tbb";
+            this.tbb.Size = new System.Drawing.Size(100, 20);
+            this.tbb.TabIndex = 7;
+            // 
+            // tbg
+            // 
+            this.tbg.Location = new System.Drawing.Point(642, 106);
+            this.tbg.Name = "tbg";
+            this.tbg.Size = new System.Drawing.Size(100, 20);
+            this.tbg.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(430, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Сдвиг";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Сдвиг_Click);
+            // 
+            // tbxy
+            // 
+            this.tbxy.Location = new System.Drawing.Point(430, 168);
+            this.tbxy.Name = "tbxy";
+            this.tbxy.Size = new System.Drawing.Size(100, 20);
+            this.tbxy.TabIndex = 10;
+            // 
+            // tbzx
+            // 
+            this.tbzx.Location = new System.Drawing.Point(430, 194);
+            this.tbzx.Name = "tbzx";
+            this.tbzx.Size = new System.Drawing.Size(100, 20);
+            this.tbzx.TabIndex = 11;
+            // 
+            // tbxz
+            // 
+            this.tbxz.Location = new System.Drawing.Point(642, 168);
+            this.tbxz.Name = "tbxz";
+            this.tbxz.Size = new System.Drawing.Size(100, 20);
+            this.tbxz.TabIndex = 12;
+            // 
+            // tbyx
+            // 
+            this.tbyx.Location = new System.Drawing.Point(536, 168);
+            this.tbyx.Name = "tbyx";
+            this.tbyx.Size = new System.Drawing.Size(100, 20);
+            this.tbyx.TabIndex = 13;
+            // 
+            // tbzy
+            // 
+            this.tbzy.Location = new System.Drawing.Point(642, 194);
+            this.tbzy.Name = "tbzy";
+            this.tbzy.Size = new System.Drawing.Size(100, 20);
+            this.tbzy.TabIndex = 14;
+            // 
+            // tbyz
+            // 
+            this.tbyz.Location = new System.Drawing.Point(536, 194);
+            this.tbyz.Name = "tbyz";
+            this.tbyz.Size = new System.Drawing.Size(100, 20);
+            this.tbyz.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(430, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Перенос";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Перенос_Click);
+            // 
+            // textbxx
+            // 
+            this.textbxx.Location = new System.Drawing.Point(430, 265);
+            this.textbxx.Name = "textbxx";
+            this.textbxx.Size = new System.Drawing.Size(100, 20);
+            this.textbxx.TabIndex = 17;
+            // 
+            // textbxz
+            // 
+            this.textbxz.Location = new System.Drawing.Point(642, 265);
+            this.textbxz.Name = "textbxz";
+            this.textbxz.Size = new System.Drawing.Size(100, 20);
+            this.textbxz.TabIndex = 18;
+            // 
+            // textbxy
+            // 
+            this.textbxy.Location = new System.Drawing.Point(536, 265);
+            this.textbxy.Name = "textbxy";
+            this.textbxy.Size = new System.Drawing.Size(100, 20);
+            this.textbxy.TabIndex = 19;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(560, 332);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Вмещение";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Вмещение_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(754, 489);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textbxy);
+            this.Controls.Add(this.textbxz);
+            this.Controls.Add(this.textbxx);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.tbyz);
+            this.Controls.Add(this.tbzy);
+            this.Controls.Add(this.tbyx);
+            this.Controls.Add(this.tbxz);
+            this.Controls.Add(this.tbzx);
+            this.Controls.Add(this.tbxy);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.tbg);
+            this.Controls.Add(this.tbb);
+            this.Controls.Add(this.tba);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tbz);
+            this.Controls.Add(this.tby);
+            this.Controls.Add(this.tbx);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbx;
+        private System.Windows.Forms.TextBox tby;
+        private System.Windows.Forms.TextBox tbz;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tba;
+        private System.Windows.Forms.TextBox tbb;
+        private System.Windows.Forms.TextBox tbg;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox tbxy;
+        private System.Windows.Forms.TextBox tbzx;
+        private System.Windows.Forms.TextBox tbxz;
+        private System.Windows.Forms.TextBox tbyx;
+        private System.Windows.Forms.TextBox tbzy;
+        private System.Windows.Forms.TextBox tbyz;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textbxx;
+        private System.Windows.Forms.TextBox textbxz;
+        private System.Windows.Forms.TextBox textbxy;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Lab rabota 1/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_rabota_1
+{
+    public partial class Form1 : Form
+
+    {
+        class Point
+        {
+            public double x;
+            public double y;
+            public double z;
+            public double H;
+
+            static public Point Parse(string str)
+            {
+                Point point = new Point();
+
+                string[] st = str.Split(' ');
+
+                point.x = double.Parse(st[0]);
+                point.y = double.Parse(st[1]);
+                point.z = double.Parse(st[2]);
+                point.H = double.Parse(st[3]);
+
+                return point;
+            }
+        }
+
+        class Line
+        {
+            public int begin;
+            public int end;
+
+            static public Line Parse(string str)
+            {
+                Line line = new Line();
+
+                string[] a = str.Split(' ');
+
+                line.begin = int.Parse(a[0]);
+                line.end = int.Parse(a[1]);
+
+                return line;
+            }
+        }
+
+        List<Point> fp = new List<Point>();
+        List<Line> fl = new List<Line>();
+
+
+        Graphics g;
+
+        void Reading()
+        {
+            StreamReader a = new StreamReader("C:\\Users\\konno\\Desktop\\работы в с\\Програмирование\\кг\\points.txt");
+
+            var str = a.ReadLine();
+            while (true)
+            {
+
+                if (a.EndOfStream) break;
+                if (str == "*") break;
+                fp.Add(Point.Parse(str));
+                str = a.ReadLine();
+            }
+            str = a.ReadLine();
+            while (true)
+            {
+
+                if (a.EndOfStream) break;
+                if (str == "*") break;
+                fl.Add(Line.Parse(str));
+                str = a.ReadLine();
+            }
+
+        }
+
+        void Accommodation()
+        {
+            double max1 = fp[0].x;
+            double min1 = fp[0].x;
+            double max2 = fp[0].y;
+            double min2 = fp[0].y;
+
+            for (int i = 0; i < fp.Count; i++)
+            {
+                if (fp[i].x > max1) max1 = fp[i].x;
+                if (fp[i].y > max2) max2 = fp[i].y;
+
+                if (fp[i].x < min1) min1 = fp[i].x;
+                if (fp[i].y < min2) min2 = fp[i].y;
+            }
+
+            double q;
+
+            if (pictureBox1.Height / (max2 - min2) > pictureBox1.Width / (max1 - min1))
+                q = pictureBox1.Width / (max1 - min1);
+            else q = pictureBox1.Height / (max2 - min2);
+
+
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].x -= min1;
+                fp[i].y -= min2;
+                
+                fp[i].x *= q;
+                fp[i].y *= q;
+                fp[i].z *= q;
+            }
+        }
+
+        void Drowing()
+        {
+            g.Clear(Color.White);
+
+            {
+                for (int i = 0; i < fl.Count; i++)
+                {
+                    g.DrawLine(new Pen(Color.Black, 2),
+                        (float)fp[fl[i].begin - 1].x, (float)fp[fl[i].begin - 1].y,
+                        (float)fp[fl[i].end - 1].x, (float)fp[fl[i].end - 1].y);
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        void Displacement(double x1, double y1, double z1)
+        {
+
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].x = fp[i].x + fp[i].H * x1;
+                fp[i].y = fp[i].y + fp[i].H * y1;
+                fp[i].z = fp[i].z + fp[i].H * z1;
+            }
+        }
+
+        void Scaling(double x2, double y2, double z2)
+        {
+
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].x = fp[i].x * x2;
+                fp[i].y = fp[i].y * y2;
+                fp[i].z = fp[i].z * z2;
+            }
+        }
+
+        void Turn(double alp, double bet, double gam)
+        {
+
+            double newX, newY, newZ;
+            for (int i = 0; i < fp.Count; i++)
+            {
+                newY = fp[i].y * Math.Cos(alp * 180 / Math.PI) - fp[i].z * Math.Sin(alp * 180 / Math.PI);
+                newZ = fp[i].y * Math.Sin(alp * 180 / Math.PI) + fp[i].z * Math.Cos(alp * 180 / Math.PI);
+                fp[i].y = newY;
+                fp[i].z = newZ;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                newX = fp[i].x * Math.Cos(bet * 180 / Math.PI) + fp[i].z * Math.Sin(bet * 180 / Math.PI);
+                newZ = -fp[i].x * Math.Sin(bet * 180 / Math.PI) + fp[i].z * Math.Cos(bet * 180 / Math.PI);
+                fp[i].x = newX;
+                fp[i].z = newZ;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                newY = fp[i].x * Math.Sin(gam * 180 / Math.PI) + fp[i].y * Math.Cos(gam * 180 / Math.PI);
+                newX = fp[i].x * Math.Cos(gam * 180 / Math.PI) - fp[i].y * Math.Sin(gam * 180 / Math.PI);
+                fp[i].y = newY;
+                fp[i].x = newX;
+            }
+        }
+
+        void Movement(double xy, double xz, double yz, double zx, double yx, double zy)
+        {
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].x = fp[i].x + fp[i].y * xy;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].x = fp[i].x + fp[i].z * xz;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].y = fp[i].y + fp[i].x * yx;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].y = fp[i].y + fp[i].z * yz;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].z = fp[i].z + fp[i].x * zx;
+            }
+            for (int i = 0; i < fp.Count; i++)
+            {
+                fp[i].x = fp[i].z + fp[i].y * zy;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+
+            Reading();
+
+            Accommodation();
+
+            Drowing();
+        }
+
+        private void Масштаб_Click(object sender, EventArgs e)
+        {
+            Scaling(Double.Parse(tbx.Text),
+                Double.Parse(tby.Text),
+                Double.Parse(tbz.Text));
+            Drowing();
+        }
+
+        private void Поворот_Click(object sender, EventArgs e)
+        {
+            Turn(Double.Parse(tba.Text),
+                  Double.Parse(tbb.Text),
+                  Double.Parse(tbg.Text));
+            Drowing();
+        }
+
+        private void Сдвиг_Click(object sender, EventArgs e)
+        {
+            Movement(Double.Parse(tbxy.Text),
+                              Double.Parse(tbyx.Text),
+                              Double.Parse(tbxz.Text),
+                              Double.Parse(tbzx.Text),
+                              Double.Parse(tbyz.Text),
+                              Double.Parse(tbzy.Text));
+            Drowing();
+        }
+
+        private void Перенос_Click(object sender, EventArgs e)
+        {
+            Displacement(Double.Parse(textbxx.Text),
+                Double.Parse(textbxy.Text),
+                Double.Parse(textbxz.Text));
+            Drowing();
+        }
+
+        private void Вмещение_Click(object sender, EventArgs e)
+        {
+           
+            Accommodation();
+
+            Drowing();
+            
+        }
+
+    }
+}
